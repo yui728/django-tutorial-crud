ba18e54ab7e37c9cc819b40d055bff01522259b5
yui728 <yui.nishimura.0728@gmail.com>
2019-07-18 17:49:15 +0900

    CRUDのモデルを作成

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return u"{0}:{1}...".format(self.id, self.message[:10])
