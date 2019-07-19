efd00e4cc34bdb069620cc6c53ddfb5eb9bc958a
yui728 <yui.nishimura.0728@gmail.com>
2019-07-19 15:54:03 +0900

    一覧表示画面を実装

--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
 
 def index(request):
-    return render(request, 'crud/index.html', {})
+    d = {
+        'messages': Message.objects.all(),
+    }
+    return render(request, 'crud/index.html', d)
 
 def add(request):
     return render(request, 'crud/edit.html', {})
--- tests/test_crud_app.py
+++ tests/test_crud_app.py
@@ -2,11 +2,17 @@ from django.test import TestCase
 
 class CrudAppTest(TestCase):
 
+    fixtures =["crud_testdata.json"] 
+
     def test_index_01(self):
         response = self.client.get('/crud/')
         # print("\nresponse:\n{}".format(response))
         self.assertTemplateUsed(response, 'crud/index.html')
-        self.assertContains(response, "index")
+        # self.assertContains(response, "index")
+        print("\nresponse:\n{}".format(response.content))
+        self.assertContains(response, r"<thead><tr><th>message</th><th>created_at</th><th>updated_at</th></tr></thead>", html=True)
+        self.assertContains(response, r"<tr><td>Test Message01</td><td>2019年7月19日0:00</td><td>2019年7月19日0:05</td></tr>", html=True)
+        self.assertContains(response, r"<tr><td>Test Message02</td><td>2019年7月19日1:00</td><td>2019年7月19日1:05</td></tr>", html=True)
 
     def test_add_01(self):
         response = self.client.get('/crud/add/')
