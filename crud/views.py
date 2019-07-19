a186936ea9d3b297bb5b1382d2c8ebcc89f9a9e7
yui728 <yui.nishimura.0728@gmail.com>
2019-07-19 17:27:07 +0900

    登録画面を作成

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Message
+from .forms import MessageForm
 
 def index(request):
     d = {
@@ -9,7 +10,15 @@ def index(request):
     return render(request, 'crud/index.html', d)
 
 def add(request):
-    return render(request, 'crud/edit.html', {})
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        Message.objects.create(**form.cleaned_data)
+        return redirect('crud:index')
+    
+    d = {
+        'form': form,
+    }
+    return render(request, 'crud/edit.html',d)
 
 def edit(request, editting_id):
     return render(request, 'crud/edit.html', {})
--- tests/test_crud_app.py
+++ tests/test_crud_app.py
@@ -9,22 +9,38 @@ class CrudAppTest(TestCase):
         # print("\nresponse:\n{}".format(response))
         self.assertTemplateUsed(response, 'crud/index.html')
         # self.assertContains(response, "index")
-        print("\nresponse:\n{}".format(response.content))
+        # print("\nresponse:\n{}".format(response.content))
         self.assertContains(response, r"<thead><tr><th>message</th><th>created_at</th><th>updated_at</th></tr></thead>", html=True)
         self.assertContains(response, r"<tr><td>Test Message01</td><td>2019年7月19日0:00</td><td>2019年7月19日0:05</td></tr>", html=True)
         self.assertContains(response, r"<tr><td>Test Message02</td><td>2019年7月19日1:00</td><td>2019年7月19日1:05</td></tr>", html=True)
+        self.assertContains(response, r'<a href="/crud/add/">登録画面へ</a>', html=True)
 
     def test_add_01(self):
         response = self.client.get('/crud/add/')
-        # print("\nresponse:\n{}".format(response))
+        # print("\nresponse:\n{}".format(response.content))
         self.assertTemplateUsed(response, 'crud/edit.html')
-        self.assertContains(response, "edit")
+        # self.assertContains(response, "edit")
+        self.assertContains(response, r'<a href="/crud/">一覧画面へ</a>')
+        self.assertContains(response, r'<label>メッセージ<input type="text" name="message" maxlength="255" required id="id_message"></label>')
+        self.assertNotContains(response, r'<ul class="errorlist">', html=True)
+
+    def test_add_02(self):
+        response = self.client.post('/crud/add/', {"message": "Test Message03"})
+        self.assertRedirects(response, '/crud/')
+
+    def test_add_03(self):
+        response = self.client.post('/crud/add/', {"message": ""})
+        print("\nresponse:\n{}".format(response.content))
+        self.assertTemplateUsed(response, 'crud/edit.html')
+        self.assertContains(response, r'<a href="/crud/">一覧画面へ</a>')
+        self.assertContains(response, r'<label>メッセージ<input type="text" name="message" maxlength="255" required id="id_message"></label>')
+        # self.assertInHTML(r'<ul class="errorlist">', response.content)
 
     def test_edit_01(self):
         response = self.client.get('/crud/edit/1')
         # print("\nresponse:\n{}".format(response))
         self.assertTemplateUsed(response, 'crud/edit.html')
-        self.assertContains(response, "edit")
+        # self.assertContains(response, "edit")
 
     def test_delete_01(self):
         response = self.client.get('/crud/delete/')
